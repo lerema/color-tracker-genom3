c3505e57db86f64ffe19eeb11f33a29986163a04
franklinselva <franklinselva10@gmail.com>
2022-10-24 15:58:18 +0200

    initialize color tracker genom3 module

---
+++ codels/CT_drone_codels.c
@@ -0,0 +1,35 @@
+
+#include "acCT_drone.h"
+
+#include "CT_drone_c_types.h"
+
+
+/* --- Function set_color ----------------------------------------------- */
+
+/** Codel SetColor of function set_color.
+ *
+ * Returns genom_ok.
+ * Throws CT_drone_e_OPENCV_ERROR.
+ */
+genom_event
+SetColor(const or_ColorTrack_ColorInfo *color,
+         const genom_context self)
+{
+  /* skeleton sample: insert your code */
+  /* skeleton sample */ return genom_ok;
+}
+
+
+/* --- Function get_color ----------------------------------------------- */
+
+/** Codel GetColor of function get_color.
+ *
+ * Returns genom_ok.
+ * Throws CT_drone_e_OPENCV_ERROR.
+ */
+genom_event
+GetColor(or_ColorTrack_ColorInfo *color, const genom_context self)
+{
+  /* skeleton sample: insert your code */
+  /* skeleton sample */ return genom_ok;
+}
